0274b937ec9794b00d9f5ac6c62feffdce14ece0
Hong <sh508@compute-a-16-167.o2.rc.hms.harvard.edu>
2023-06-02 15:14:05 -0400

    test if package is installable

---
+++ scAI_SNP.py
@@ -0,0 +1,4 @@
+def test_mic():
+	print('mic test one two three')
+
+__version__ = 'dev'
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from scAI_SNP import __version__
+
+setup(
+	name='scAI_SNP',
+	version=__version__,
+
+	url='https://github.com/hongdavid94/ancestry',
+	author='Sung Chul (David) Hong',
+	author_email='hongdavid852@gmail.com',
+
+	py_modules=['scAI_SNP'],
+)
