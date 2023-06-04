12c1da45d59b356c942f7b59c1ace6d25d0384e4
Hong <sh508@compute-e-16-233.o2.rc.hms.harvard.edu>
2023-06-03 20:01:23 -0400

    added centering function

---
+++ scAI_SNP/commands.py
@@ -0,0 +1,29 @@
+import typer
+import numpy as np
+
+from scAI_SNP.math import read_center
+from scAI_SNP.math import read_validate
+
+app = typer.Typer(
+	help = "Command line tool to extract genetic population classification of mutation data "
+		"using HGDP project"
+)
+
+@app.command(short_help="classify the data")
+def classify(input):
+
+	col_mean = read_center()
+	col_input = read_validate(input)
+	
+	# centered column that ignores NA (np.nan)
+	col_center = col_input - col_mean
+	mask = np.isnan(col_center)
+	col_center[mask] = col_mean[mask]
+
+	print(f"the input had {np.isnan(col_input)} NA values")
+	print(f"the output had {np.isnan(col_center)} NA values")
+
+	print(f"the input had {np.nanmean(col_input)} average")
+	print(f"the output had {np.nanmean(col_center)} average")
+
+	return None
--- scAI_SNP/math.py
+++ scAI_SNP/math.py
@@ -1,3 +1,5 @@
+import sys
+import os
 import numpy as np
 import pandas as pd
 from returns import returns
@@ -50,5 +52,15 @@ def read_validate(file):
 	if mask.any():
 		print(f"ERROR (INPUT): Found invalid values, entries must be {0, 0.5, 1, NA}")
 		return None
-
+	
 	return data_values
+
+# a function that reads mean genotype of the training data
+def read_center():
+	try:
+		file_mean = 'data/genotype_mean.col'
+		return pd.read_csv(file_mean, header = None)
+	except FileNotFoundError:
+		print("ERROR (PACKAGE): mean genotype of training not found")
+		print(f"current directory: {os.getcwd()}")
+		sys.exit(1)
