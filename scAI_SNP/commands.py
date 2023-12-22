9e73744cad416ed7e4b9294949e6139c4a93a5ff
Hong <sh508@compute-a-16-161.o2.rc.hms.harvard.edu>
2023-12-21 21:18:48 -0500

    update to classificaiton to convex

--- scAI_SNP/commands.py
+++ scAI_SNP/commands.py
@@ -45,46 +45,91 @@ def classify(file_input, path_output, name_input = None, bool_save_plot = True):
 	print("SUCCESS: centering complete!")
 	
  	## original
-	print("reading LDA projection matrix...")
-	mat_proj_lda = pd.read_csv('data/proj_lda.tsv.gz', sep = '\t', compression = 'gzip', header = None).values
-	print("SUCCESS: projection matrix loaded!")
-	print("applying classification...")
- 
+	# print("reading LDA projection matrix...")
+	# mat_proj_lda = pd.read_csv('data/proj_lda.tsv.gz', sep = '\t', compression = 'gzip', header = None).values
+	# print("SUCCESS: projection matrix loaded!")
+	# print("applying classification...")
+	
  	## new
- 	print("reading PCA projection matrix...")
+	print("reading PCA projection matrix...")
+	print(f"cp version: {cp.__version__}")
 	# mat_proj_pca = pd.read_csv('data/proj_pca.tsv.gz', sep = '\t', compression = 'gzip', header = None).values
+	mat_proj_pca = pd.read_csv(f'data/proj_PCA/mat_proj_PCA_cc1.tsv.gz', compression = 'gzip', sep = '\t', header = None).values
+	print(f'shape of mat_proj_pca (4.5M by 100): {mat_proj_pca.shape}')
 	print("SUCCESS: PCA projection matrix loaded!")
-	print("applying classification...")
+
 	
  	## original
-	lda_proj_input = mat_input_centered_scaled.T @ mat_proj_lda
+	# lda_proj_input = mat_input_centered_scaled.T @ mat_proj_lda
 	## new
- 	# pca_projected_input = mat_input_centered_scaled.T @ mat_proj_pca
+ 
+	print("reading mean PCA matrix...")
+	mat_mean_PC = pd.read_csv(f'data/mat_GT_PCA_projected_mean.tsv', sep = '\t', header = None).values
+	mat_mean_PC = mat_mean_PC[0:100,:]	
+	print(f'shape of mat_mean_P (100 by 26): {mat_mean_PC.shape}')
+	print("SUCCESS: mean PCA matrix loaded!")
+ 
+	
+	print("applying PCA...")
+	print(f'shape of mat_input_centered_scaled (4.5M by n_sample): {mat_input_centered_scaled.shape}')
+	pca_projected_input = mat_input_centered_scaled.T @ mat_proj_pca
+	print("SUCCESS: PCA applied!")
  
 	## original
-	coef_lr = pd.read_csv('model/LR/LR_coef.tsv', sep = '\t', header = None).to_numpy()
-	intercept_lr = pd.read_csv('model/LR/LR_intercept.tsv', sep = '\t', header = None).to_numpy()
-	vec_population = pd.read_csv('model/LR/population.tsv', header = None).to_numpy().flatten()
+	# coef_lr = pd.read_csv('model/LR/LR_coef.tsv', sep = '\t', header = None).to_numpy()
+	# intercept_lr = pd.read_csv('model/LR/LR_intercept.tsv', sep = '\t', header = None).to_numpy()
+	# vec_population = pd.read_csv('model/LR/population.tsv', header = None).to_numpy().flatten()
 	
-	scores = lda_proj_input @ coef_lr.T + intercept_lr.T
+	# scores = lda_proj_input @ coef_lr.T + intercept_lr.T
 	
-	print(f"scores dimension: {scores.shape}")
-	index_max = np.argmax(scores, axis = 1)
-	prob = np.exp(scores) / np.sum(np.exp(scores), axis = 1, keepdims = True)
+	# print(f"scores dimension: {scores.shape}")
+	# index_max = np.argmax(scores, axis = 1)
+	# prob = np.exp(scores) / np.sum(np.exp(scores), axis = 1, keepdims = True)
 
-	for sample in range(n_sample):
-		print(f"For sample {sample + 1}:")
-		print(f"classified population: {vec_population[index_max[sample]]}")
-		print(f"prob: {prob[sample, index_max[sample]]}")
+	#for sample in range(n_sample):
+	#	print(f"For sample {sample + 1}:")
+	#	print(f"classified population: {vec_population[index_max[sample]]}")
+	#	print(f"prob: {prob[sample, index_max[sample]]}")
 	## new
 	# under construction
  
+	def predict_convex(test_vec, mean_vectors = mat_mean_PC):
+		# Define the optimization variable
+		X = cp.Variable(26)
+		# Define the objective function (minimize the least squares error)
+		objective = cp.Minimize(cp.norm(mean_vectors @ X - test_vec, 'fro'))
+		# Define the constraints
+		constraints = [X >= 0, cp.sum(X) == 1]
+
+		# Define and solve the problem
+		problem = cp.Problem(objective, constraints)
+		problem.solve()
+		this_predicted = X.value
+
+		# Use this_predicted to construct a prediction vector from the mean_vectors
+		vec_predicted = mean_vectors @ this_predicted
+
+		# Compute the cosine similarity between this_vector and test_vec
+		cos_sim = np.dot(vec_predicted, test_vec) / (np.linalg.norm(vec_predicted) * np.linalg.norm(test_vec))  # cosine similarity is computed using this single line of code ***
+
+		return this_predicted, cos_sim
+
+	print("applying classification...")
+	list_predicted = []
+	list_angle = []
+	for index_sample in range(n_sample):
+		vec_prob, angle = predict_convex(pca_projected_input[index_sample,:].T)
+		list_predicted.append(vec_prob)
+		list_angle.append(angle)
+	
 	print("SUCCESS: classification done!")
 	print(f"classify took {round((time.time() - now)/60, 2)} minutes")
-	print("plotting and saving probabilities...")
+	print("Saving probabilities...")
 
 	if (n_sample == 1):
-		prob = prob.reshape(1, -1)
+		prob = vec_prob.reshape(1, -1)
+	else:
+		prob = np.array(list_predicted)
 
 	df_prob = pd.DataFrame(prob.T, index = vec_population, columns = vec_name_input)
 	print(f"saving probabilities to output/probabilities.tsv")
